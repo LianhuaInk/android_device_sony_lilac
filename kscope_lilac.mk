2daff817e9b10607c489f45ff2517e7c9f90ea58
LianhuaInk <63184806+LianhuaInk@users.noreply.github.com>
2025-12-23 19:48:52 +0800

    Update and rename lineage_lilac.mk to kscope_lilac.mk

--- kscope_lilac.mk
+++ kscope_lilac.mk
@@ -9,17 +9,17 @@ $(call inherit-product, device/sony/lilac/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
 ### BOOTANIMATION
-# vendor/lineage/config/common_full_phone.mk
+# vendor/kscope/config/common_full_phone.mk
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
-# vendor/lineage/config/common.mk
+# vendor/kscope/config/common.mk
 TARGET_BOOTANIMATION_HALF_RES := true
 
-### LINEAGE
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+### kscope
+$(call inherit-product, vendor/kscope/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_lilac
+PRODUCT_NAME := kscope_lilac
 PRODUCT_DEVICE := lilac
 PRODUCT_BRAND := Sony
 PRODUCT_MODEL := G8441
